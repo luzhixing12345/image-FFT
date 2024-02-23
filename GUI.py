f3312b14210a8b932c08031825f15beaa58fc455
luzhixing12345 <luzhixing12345@163.com>
2024-02-23 22:54:02 +0800

    [fix] : fix run before load image in GUI, remove some useless images

--- GUI.py
+++ GUI.py
@@ -11,6 +11,7 @@ from opencvUtils import *
 class InteractiveWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.fname = None
 
         # configuration
         self.setWindowTitle("Visiable FFT image")
@@ -68,6 +69,9 @@ class InteractiveWindow(QWidget):
         self.fname, _ = QFileDialog.getOpenFileName(
             self, "选择图片", "G:\\learnerLu\\image-FFT\\examples", "Image files(*.jpg *.gif *.png *.jpeg *.svg)"
         )
+        if self.fname == "":
+            self.fname = None
+            return
         # 在l1里面,调用setPixmap命令,建立一个图像存放框,并将之前的图像png存放在这个框框里.
         self.picture.setPixmap(QPixmap(self.fname))
 
@@ -75,23 +79,36 @@ class InteractiveWindow(QWidget):
         """
         use th selected method to process the image to get the edge
         """
+        if self.fname is None:
+            msg_box = QMessageBox()
+            msg_box.setText("请先导入图片")
+            msg_box.setStandardButtons(QMessageBox.Ok)
+            msg_box.exec_()
+            return
+
         processMethod = self.edgeProcessingMethod[self.edgeProcessingMethodChoose.currentText()]
 
-        self.newPictureName = processMethod(self.fname)
+        self.processed_img = processMethod(self.fname)
         # print(self.newPictureName)
-        self.picture.setPixmap(QPixmap(self.newPictureName))
+        self.picture.setPixmap(QPixmap(self.processed_img))
 
     def fftImageProcess(self):
-        self.newPictureName = f"./images/{self.fname.split('/')[-1].split('.')[0]}.svg"
-        fileName = f"./images/{self.fname.split('/')[-1].split('.')[0]}.txt"
+        if self.fname is None:
+            msg_box = QMessageBox()
+            msg_box.setText("请先导入图片")
+            msg_box.setStandardButtons(QMessageBox.Ok)
+            msg_box.exec_()
+            return
 
+        self.svg_name = f"./images/{self.fname.split('/')[-1].split('.')[0]}.svg"
+
+        self.processImage()
         if self.fname.split(".")[-1] != "svg":
-            bitmap_to_contour_svg(self.fname, self.newPictureName)
+            bitmap_to_contour_svg(self.processed_img, self.svg_name)
         else:
-            self.newPictureName = self.fname
-        self.picture.setPixmap(QPixmap(self.newPictureName))
-        # fftProcess(fileName)
-        draw(self.newPictureName)
+            self.svg_name = self.fname
+        self.picture.setPixmap(QPixmap(self.svg_name))
+        draw(self.svg_name)
 
 
 if __name__ == "__main__":
